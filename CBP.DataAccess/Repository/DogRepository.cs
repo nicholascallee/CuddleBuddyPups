edfd3c2442030d66685a7592a1fb9b3a3a7a95bd
nicholascallee <nicholascallee@gmail.com>
2023-10-17 23:03:54 -0500

    removed migrations and pointed to a new db for the cbp site.

--- CBP.DataAccess/Repository/DogRepository.cs
+++ CBP.DataAccess/Repository/DogRepository.cs
@@ -1,11 +1,6 @@
 ﻿using CBP.DataAccess.Data;
 using CBP.DataAccess.Repository.IRepository;
 using CBP.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CBP.DataAccess.Repository
 {
